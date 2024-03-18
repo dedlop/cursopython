768136d80761488e3a52d45d05ed9f9020f3d29b
dedlop <104914772+dedlop@users.noreply.github.com>
2024-03-18 11:59:24 -0300

    Update aula 76

--- aula76.py
+++ aula76.py
@@ -13,20 +13,43 @@
 
 #pessoa = dict(nome='Luiz Otávio', sorenome='Miranda')
 
-pessoa = {
-    'nome': 'Luiz Otávio',
-    'sobrenome': 'Miranda',
-    'idade': 18,
-    'altura': 1.8,
-    'endereços': [
-        {'rua': 'tal tal', 'número': 123},
-        {'rua': 'outra rua', 'número': 321}
-    ],
-}
-# print(pessoa, type(pessoa))
-print(pessoa['nome'])
-print(pessoa['sobrenome'])
-print()
+# pessoa = {
+#     'nome': 'Luiz Otávio',
+#     'sobrenome': 'Miranda',
+#     'idade': 18,
+#     'altura': 1.8,
+#     'endereços': [
+#         {'rua': 'tal tal', 'número': 123},
+#         {'rua': 'outra rua', 'número': 321}
+#     ],
+# }
+# # print(pessoa, type(pessoa))
+# print(pessoa['nome'])
+# print(pessoa['sobrenome'])
+# print()
 
-for chave in pessoa:
-    print(chave, pessoa[chave])
+# for chave in pessoa:
+#     print(chave, pessoa[chave])
+
+# Manipulando chaves e valores em dicionários
+pessoa = {}
+
+chave = 'nome'
+
+pessoa[chave] = 'Luiz Otávio'
+pessoa['sobrenome'] = 'Miranda'
+
+print(pessoa[chave])
+
+pessoa[chave] = 'Maria'
+
+del pessoa['sobrenome']
+print(pessoa)
+print(pessoa['nome'])
+
+#print(pessoa.get('sobrenome'))
+if pessoa.get('sobrenome') is None:
+    print('Não Existe')
+else:
+    print(pessoa['sobrenome'])
+#print('Isso não vai')
